4139c0357f3bd3f93764082ab9a06fcac637ad3a
PierluigiLombardo <pigilombardo2002@gmail.com>
2024-10-09 09:31:24 +0200

    added nextUI library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const { nextui } = require("@nextui-org/react");
+
 module.exports = {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}", // Percorsi ai file React
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
