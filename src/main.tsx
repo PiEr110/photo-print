c9c6884e1e73a8311e487dc1ac25960e6772326b
PierluigiLombardo <pigilombardo2002@gmail.com>
2024-10-14 14:27:21 +0200

    aggiunte alcune modifiche

--- src/main.tsx
+++ src/main.tsx
@@ -5,37 +5,36 @@ import App from "./App.tsx";
 import "./index.css";
 
 //Esempio di come potrebbe funzionare Shadow DOM
+const hostElement = document.getElementById("root-react"); //elemento root generato da wordpress
+if (hostElement) {
+  // Creazione di uno Shadow Roor con modalità "open"
+  const shadowRoot = hostElement.attachShadow({ mode: "open" });
 
-// const hostElement = document.getElementById("root"); //elemento root generato da wordpress
-// if (hostElement) {
-//   // Creazione di uno Shadow Roor con modalità "open"
-//   const shadowRoot = hostElement.attachShadow({ mode: "open" });
+  // Creazione di un div per montare l'app React
+  const reactContainer = document.createElement("div");
+  shadowRoot.appendChild(reactContainer);
 
-//   // Creazione di un div per montare l'app React
-//   const reactContainer = document.createElement("div");
-//   shadowRoot.appendChild(reactContainer);
+  // Iniettare gli stili nello Shadow Dom
+  const style = document.createElement("style");
+  style.textContent = `@import url('dist/assets/index-CmZCYIxa.css')`;
+  shadowRoot.appendChild(style);
 
-//   // Iniettare gli stili nello Shadow Dom
-//   const style = document.createElement("style");
-//   style.textContent = `@import url('${plugin_dir_url}/react-integration/build/static/css/main.css')`;
-//   shadowRoot.appendChild(style);
+  // Montiamo l'app React nel contenitore
+  createRoot(reactContainer).render(
+    <StrictMode>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </StrictMode>,
+  );
+} else {
+  console.error("Elemento con id 'root' non trovato");
+}
 
-//   // Montiamo l'app React nel contenitore
-//   createRoot(reactContainer).render(
-//     <StrictMode>
-//       <NextUIProvider>
-//         <App />
-//       </NextUIProvider>
-//     </StrictMode>,
-//   );
-// } else {
-//   console.error("Elemento con id 'root' non trovato");
-// }
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </StrictMode>,
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <NextUIProvider>
+//       <App />
+//     </NextUIProvider>
+//   </StrictMode>,
+// );
