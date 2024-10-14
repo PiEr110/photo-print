d1246247e22692621ecb437ffc3e618dd0786bc2
PierluigiLombardo <pigilombardo2002@gmail.com>
2024-10-14 17:20:45 +0200

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -4,37 +4,39 @@ import { NextUIProvider } from "@nextui-org/react";
 import App from "./App.tsx";
 import "./index.css";
 
-//Esempio di come potrebbe funzionare Shadow DOM
-const hostElement = document.getElementById("root-react"); //elemento root generato da wordpress
-if (hostElement) {
-  // Creazione di uno Shadow Roor con modalità "open"
-  const shadowRoot = hostElement.attachShadow({ mode: "open" });
+// //Esempio di come potrebbe funzionare Shadow DOM
+// const hostElement = document.getElementById("root-react"); //elemento root generato da wordpress
+// if (hostElement) {
+//   // Creazione di uno Shadow Roor con modalità "open"
+//   const shadowRoot = hostElement.attachShadow({ mode: "open" });
 
-  // Creazione di un div per montare l'app React
-  const reactContainer = document.createElement("div");
-  shadowRoot.appendChild(reactContainer);
+//   // Creazione di un div per montare l'app React
+//   const reactContainer = document.createElement("div");
+//   shadowRoot.appendChild(reactContainer);
 
-  // Iniettare gli stili nello Shadow Dom
-  const style = document.createElement("style");
-  style.textContent = `@import url('dist/assets/index-CmZCYIxa.css')`;
-  shadowRoot.appendChild(style);
+//   // Iniettare gli stili nello Shadow Dom
+//   const style = document.createElement("style");
+//   // style.textContent =
+//   //   document.querySelector("style[data-tailwind]")?.textContent ?? "";
+//   style.textContent = `@import url('./src/index.css')`;
+//   shadowRoot.appendChild(style);
 
-  // Montiamo l'app React nel contenitore
-  createRoot(reactContainer).render(
-    <StrictMode>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </StrictMode>,
-  );
-} else {
-  console.error("Elemento con id 'root' non trovato");
-}
+//   // Montiamo l'app React nel contenitore
+//   createRoot(reactContainer).render(
+//     <StrictMode>
+//       <NextUIProvider>
+//         <App />
+//       </NextUIProvider>
+//     </StrictMode>,
+//   );
+// } else {
+//   console.error("Elemento con id 'root' non trovato");
+// }
 
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <NextUIProvider>
-//       <App />
-//     </NextUIProvider>
-//   </StrictMode>,
-// );
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </StrictMode>,
+);
